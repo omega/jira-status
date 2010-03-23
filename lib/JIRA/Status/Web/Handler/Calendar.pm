3a1fbf23be2b65cea590f446f7661a4b0486a56c
Andreas Marienborg <andreas.marienborg@gmail.com>
2010-03-23 14:32:38 +0100

    Refactoring calendar view and handler to make it easier to sub-class

--- lib/JIRA/Status/Web/Handler/Calendar.pm
+++ lib/JIRA/Status/Web/Handler/Calendar.pm
@@ -1,19 +1,27 @@
 package JIRA::Status::Web::Handler::Calendar;
-use base qw(JIRA::Status::Web::Handler);
+use Moose;
+extends 'JIRA::Status::Web::Handler';
 
 use DateTime;
 
+
 sub get {
     my $self = shift;
-    # 201003 for instance (which is march 2010)
+    $self->render('cal.tt', $self->get_args);
+}
+
+
+sub get_args {
+    my $self = shift;
     my $now = DateTime->now();
+    # 201003 for instance (which is march 2010)
     my ($y, $m) = ( ($self->request->parameters->get('month') || $now->strftime('%Y%m')) =~ m/(\d{4})(\d{2})/ );
     
     my $date = DateTime->new({ month => $m, year => $y});
     
     my $issues = $self->model->get_active_releases_by_month($date);
     
-    my $args = {
+    {
         issues => $issues,
         statuses => $self->model->get_status_list,
         date => $date,
@@ -21,8 +29,7 @@ sub get {
         next => $date->clone->add(months => 1),
         today => $now,
     };
-    $self->render('cal.tt', $args);
+    
 }
-
 
 1;
