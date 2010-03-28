fe18a8c5ac1c77d93470d9a70511637269cca642
Andreas Marienborg <andreas.marienborg@gmail.com>
2010-03-28 22:37:07 +0200

    Remove an unused module

--- lib/JIRA/Status/Data/Events/EventSet.pm
+++ lib/JIRA/Status/Data/Events/EventSet.pm
@@ -9,14 +9,10 @@ class ::EventSet {
         is => 'ro', isa => ArrayOfEvents,
         default => sub { [ ] },
         handles => {
-            '_add_event' => 'push',
+            'add_event' => 'push',
             'events' => 'elements',
         },
     );
-    # We can't just delegate because we want coercion
-    method add_event(Event $event) {
-        $self->_add_event($event);
-    }
     
     method as_date_hash() {
         my $date_hash = {};
@@ -26,5 +22,6 @@ class ::EventSet {
         
         return $date_hash;
     }
+    
 }
 1;
