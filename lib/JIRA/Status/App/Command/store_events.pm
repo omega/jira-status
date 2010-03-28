7e50244715372d88fb77bc5df7e85ba2e1b61f05
Andreas Marienborg <andreas.marienborg@gmail.com>
2010-03-28 22:39:59 +0200

    Update config, and add cli-app for storing events

---
+++ lib/JIRA/Status/App/Command/store_events.pm
@@ -0,0 +1,43 @@
+package JIRA::Status::App::Command::store_events;
+use Moose;
+
+extends qw(JIRA::Status::App::Command);
+
+
+use JIRA::Status::Web::Types qw/ArrayOfEventSources/;
+use MooseX::Types::DateTime qw(DateTime);
+
+use JIRA::Status::Data::Events::EventSet;
+use JIRA::Status::Data::Events::EventSource;
+
+has 'sources' => (
+    traits => [qw/Array/],
+    is => 'ro',
+    isa => ArrayOfEventSources,
+    coerce => 1,
+    default => sub {[]},
+    handles => {
+        'all_sources' => 'elements',
+        '_grep_sources' => 'grep',
+    },
+);
+
+sub execute {
+    my ($self) = @_;
+    my $scope = $self->db->new_scope;
+
+    my $events = JIRA::Status::Data::Events::EventSet->new();
+
+    foreach my $source ($self->all_sources) {
+        warn "source: " . $source->name;
+        foreach my $event ($source->events) {
+            $events->add_event($event);
+
+        }
+
+    }    
+    $self->db->store_eventset($events);
+}
+
+
+1;
