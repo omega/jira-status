9bbdb1f1d3f49424401ddc07a6f2ef4d9d8b747a
Andreas Marienborg <andreas.marienborg@gmail.com>
2010-04-22 13:29:19 +0200

    Fix more JS crap. Man jQuery is hard!

--- static/js/more.js
+++ static/js/more.js
@@ -6,15 +6,13 @@ $(document).ready(function() {
 
 /* Bigscreen stuff */
 
-var placeHolder, refreshValue = 10;
+var placeHolder, timer;
 
-$(document).ready(function() {
-    placeHolder = $("#bigscreen");
-    if (!placeHolder) {
-        return;
-    }
-    window.setInterval(function() {
-        $.get('/bigscreen?nowrap=1', function(response, status, req) {
+function refreshCal() {
+    $.ajax({
+        cache: false,
+        url: '/bigscreen?nowrap=1', 
+        success: function(response, status, req) {
             if (req.status == 200) {
                 placeHolder.empty();
                 placeHolder.html(response);
@@ -22,6 +20,27 @@ $(document).ready(function() {
             } else {
                 $('#timestamp').addClass('error');
             }
-        });
-    }, 10000);
+        },
+        error: function(req, status) {
+            $('#timestamp').addClass('error');
+        }
+    });
+}
+$(document).ready(function() {
+    placeHolder = $("#bigscreen");
+    if (!placeHolder) {
+        return;
+    }
+    timer = window.setInterval(refreshCal, 10000);
+    
+    $('#timestamp').live('click', function() {
+        if ($(this).hasClass('stopped')) {
+            // We should 
+            $(this).removeClass('stopped');
+            timer = window.setInterval(refreshCal, 10000);
+        } else {
+            window.clearInterval(timer);
+            $(this).addClass('stopped');
+        }
+    });
 });
