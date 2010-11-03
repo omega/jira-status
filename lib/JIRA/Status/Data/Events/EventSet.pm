5a1327b8f207fc67a28d34a3e0370863a6e5da14
Andreas Marienborg <andreas.marienborg@gmail.com>
2010-11-03 18:45:01 +0100

    Whitespace cleanup

--- lib/JIRA/Status/Data/Events/EventSet.pm
+++ lib/JIRA/Status/Data/Events/EventSet.pm
@@ -3,7 +3,7 @@ namespace JIRA::Status::Data::Events;
 
 class ::EventSet {
     use JIRA::Status::Web::Types qw/Event ArrayOfEvents/;
-    
+
     has '_events' => (
         traits => [qw/Array/],
         is => 'ro', isa => ArrayOfEvents,
@@ -14,16 +14,15 @@ class ::EventSet {
             '_grep' => 'grep',
         },
     );
-    
+
     method as_date_hash() {
         my $date_hash = {};
         foreach ($self->events) {
             push(@{ $date_hash->{ $_->datetime->ymd } }, $_);
         }
-        
         return $date_hash;
     }
-    
+
     method old_events(DateTime $limit, DateTime $oldest?) {
         my $l = $limit->clone->truncate(to => 'days');
         return [$self->_grep(sub { $_->datetime < $l and (defined($oldest) ? $_->datetime > $oldest : 1) } )];
